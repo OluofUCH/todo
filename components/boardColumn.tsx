4faea2b27977723add5e53d1e7d7d81e0f3b2e4d
OluofUCH <olubiyidavid441@gmail.com>
2025-07-24 21:31:14 +0100

    build error fix

--- components/boardColumn.tsx
+++ components/boardColumn.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import Columnheader from './columnHeader' 
-import Taskcard from './taskCard'  
-
+import React from "react";
+import Columnheader from "./columnHeader";
+import Taskcard from "./taskCard";
 
 interface BoardColumnProps {
   columnId: string;
@@ -9,34 +8,40 @@ interface BoardColumnProps {
   task: Array<TaskData>;
   refresh: () => void;
   edit: (taskId: string, columnId: string) => void;
-} 
-interface TaskData {
-  title: string
-  description: string
-  category: string
-  owner: string[]
-  columnId: string
-  id: string
 }
-export default function Boardcolumn({columnId, onAdd, task, refresh, edit}: BoardColumnProps) {
-  
+interface TaskData {
+  title: string;
+  description: string;
+  category: string;
+  owner: string[];
+  columnId: string;
+  id: string;
+}
+export default function Boardcolumn({
+  columnId,
+  onAdd,
+  task,
+  refresh,
+  edit,
+}: BoardColumnProps) {
   return (
-    <div className='h-full min-h-full p-[24px] flex flex-col gap-[24px] w-full rounded-[24px] bg-[#EEF2F5]'>
-        <Columnheader columnId={columnId} onAdd={onAdd} />
-        {task.map((data: TaskData, index: number)=>(
-
-          <div key={index} className='flex flex-col gap-[16px] '>
-
-        <Taskcard {...data} edit={(taskId) => edit(data.id, data.columnId)} refresh={refresh}/>
+    <div className="h-full min-h-full p-[24px] flex flex-col gap-[24px] w-full rounded-[24px] bg-[#EEF2F5]">
+      <Columnheader columnId={columnId} onAdd={onAdd} />
+      {task.map((data: TaskData, index: number) => (
+        <div key={index} className="flex flex-col gap-[16px] ">
+          <Taskcard
+            {...data}
+            edit={() => edit(data.id, data.columnId)}
+            refresh={refresh}
+          />
         </div>
-        ))}
-        {task.length === 0 && (
-  <div className="text-lg text-black">
-    <p>No {columnId} task yet </p>
-    <p className="text-gray-500">Click the + button to add a new task.</p>
-  </div>
-)}
-
+      ))}
+      {task.length === 0 && (
+        <div className="text-lg text-black">
+          <p>No {columnId} task yet </p>
+          <p className="text-gray-500">Click the + button to add a new task.</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
